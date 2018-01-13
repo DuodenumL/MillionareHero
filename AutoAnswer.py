96ca8fe34552c02a3188adea5abf7b26cbaf8580
Duodenum <2410088750@qq.com>
2018-01-13 23:48:11 +0800

    基本功能

---
+++ AutoAnswer.py
@@ -0,0 +1,151 @@
+from aip import AipOcr
+import time
+import json
+from PIL import Image
+import requests
+import os
+import itchat
+import traceback
+
+
+class AutoAnswer:
+    APP_KEY = "your_app_key"
+    APP_ID = "your_app_id"
+    SECRET_KEY = "your_secret_key"
+    client = AipOcr(APP_ID, APP_KEY, SECRET_KEY)
+    width, height = 0, 0
+    # 在下面填入想要自动发送微信的好友微信号/昵称/备注
+    users = []
+
+    @classmethod
+    def adb_connect(cls):
+        # 网易mumu的端口号是5555，如果连接自己的手机则不需要此函数
+        os.system('adb connect 127.0.0.1:5555')
+
+    @classmethod
+    def adb_get_image(cls, filepath):
+        os.system('adb shell /system/bin/screencap -p /sdcard/screenshot.png')
+        os.system('adb pull /sdcard/screenshot.png /your/path/' + filepath)
+
+    @classmethod
+    def detect_question(cls, filepath):
+        im = Image.open(filepath)
+        global width
+        global height
+        width, height = im.size
+        # 不同机型对应的line不同，例如三星S7对应为330
+        line = [100 * width // 1080, 220 * height // 1920, 900 * width // 1080, 220 * height // 1920]
+        is_question = True
+        for x in range(line[0], line[2]):
+            if len(set(im.getpixel((x, line[1]))) - {255}) != 0:
+                is_question = False
+        return is_question, im
+
+    @classmethod
+    def cut_image(cls, im):
+        width, height = im.size
+        box = 0, 120 * height // 1920, 1080 * width // 1080, 1400 * height // 1920
+        print("box", box)
+        region = im.crop(box)
+        region.save('m3.png', 'PNG')
+
+    @classmethod
+    def get_file_content(cls, file_path):
+        with open(file_path, 'rb') as fp:
+            return fp.read()
+
+    @classmethod
+    def get_result(cls, res, question, options):
+        print("get_result options", options)
+        # 如果在服务器上运行MillionareHero.py，请将下面url替换
+        url = "http://localhost:8995/m/"
+
+        payload = json.dumps({'question': question, 'options': options})
+        headers = {
+            'content-type': "application/json",
+            'cache-control': "no-cache",
+        }
+
+        response = requests.request("POST", url, data=payload, headers=headers)
+        print(response.text)
+        data = json.loads(response.text)
+        print("data", data)
+        privative = {'不是', '不属于', '没有', '不包括', '不在', '不包含'}
+        reverse = False
+        for item in privative:
+            if item in question:
+                reverse = True
+        print("reverse", reverse)
+        if reverse:
+            result = res[data[len(data) - 1][0]]
+        else:
+            result = res[data[0][0]]
+        print("result", result)
+        top = result['location']['top'] + 120 * height // 1920
+        print('adb shell input tap %s %s' % (250, top))
+        # 自动点击，如果不需要可以去掉
+        os.system('adb shell input tap %s %s' % (250, top))
+
+        return result['words']
+
+    @classmethod
+    def get_detail(cls, res):
+        words_result = res['words_result']
+        words_result = sorted(words_result, key=lambda x: x['location']['top'])
+        question = ''
+        options = dict()
+        is_question = True
+        order = ['A', 'B', 'C', 'D', 'E']
+        for item in words_result:
+            if is_question:
+                question += item['words']
+                if question.endswith('?') or question.endswith('？'):
+                    is_question = False
+            else:
+                options[order[0]] = item
+                order = order[1:]
+        return question[2:], options
+
+    @classmethod
+    def notice_users(cls, text):
+        for user in cls.users:
+            username = itchat.search_friends(name=user)[0]['UserName']
+            itchat.send(text, toUserName=username)
+            print('send', text, ' to', user)
+
+    @classmethod
+    def auto_start(cls):
+        cls.adb_connect()
+        itchat.auto_login(True)
+        while True:
+            try:
+                start_time = time.time()
+                cls.adb_get_image('m2.png')
+                is_question, im = cls.detect_question('m2.png')
+                print("检测到问题？", is_question)
+                if not is_question:
+                    time.sleep(1)
+                    continue
+                else:
+                    cls.cut_image(im)
+                    image = cls.get_file_content("m3.png")
+                    res = cls.client.general(image)
+
+                    print(res)
+                    question, options = cls.get_detail(res)
+                    print("解析图片耗时", time.time() - start_time)
+                    print(question)
+                    print(options)
+                    result = cls.get_result(options, question, {x: options[x]['words'] for x in options})
+                    print(result)
+                    cls.notice_users(result)
+                    print("共耗时", time.time() - start_time)
+                    time.sleep(30)
+            except Exception as e:
+                traceback.print_exc()
+                # cls.notice_users('出错了...')
+            finally:
+                time.sleep(1)
+
+
+AutoAnswer.auto_start()
---
+++ MillionareHero.py
@@ -0,0 +1,126 @@
+# coding=utf-8
+import json
+from bs4 import BeautifulSoup
+import jieba
+from jieba import analyse
+import time
+import aiohttp
+from aiohttp import web
+import asyncio
+
+GOOGLE = {"url": "https://www.google.com/search?q=", "content_id": "ires"}
+BAIDU = {"url": "https://www.baidu.com/s?wd=", "content_id": "content_left"}
+
+privative = {'不是', '不属于', '没有', '不包括', '不包含'}
+rm = {'不', '没'}
+
+headers = {
+    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_3) AppleWebKit/537.36 (KHTML, like Gecko)"
+                  " Chrome/63.0.3239.84 Safari/537.36"
+}
+
+
+class MillionareHero:
+
+    def __init__(self, question, options, engine=BAIDU):
+        self.score = {k: 0 for k in options}
+        self.options = options
+        self.seg_question = MillionareHero.get_keyword(question) - privative
+        for item in rm:
+            question = question.replace(item, '')
+        self.seg_question.add(question)
+        self.question = ' '.join(self.seg_question)
+        self.engine = engine
+        print(self.question)
+
+    async def get_score(self):
+        tasks = [asyncio.ensure_future(self.search_question(self.question, self.options, self.engine))]
+        for option in self.options.items():
+            tasks.append(asyncio.ensure_future(self.search_option(self.seg_question, option, self.engine)))
+        await asyncio.gather(*tasks)
+        return self.score
+
+    async def search_option(self, seg_question, option, engine=BAIDU):
+        print("抓取选项中...", option)
+        t1 = time.time()
+        async with aiohttp.ClientSession() as session:
+            async with session.get(engine['url'] + option[1], headers=headers) as resp:
+                html = await resp.text()
+                # html = bytearray(html.content).decode("utf-8")
+                html = BeautifulSoup(html, 'html.parser').find('div', attrs={'id': engine['content_id']}).getText()
+                print(option)
+                self.score[option[0]] += MillionareHero.get_score_from_option(html, seg_question)
+                print("抓完了", option, time.time() - t1)
+
+    async def search_question(self, question, options, engine=BAIDU):
+        print("抓取问题中...", question)
+        t1 = time.time()
+        async with aiohttp.ClientSession() as session:
+            async with session.get(engine['url'] + question, headers=headers) as resp:
+                html = await resp.text()
+                # html = bytearray(html.content).decode("utf-8")
+                html = BeautifulSoup(html, 'html.parser').find('div', attrs={'id': engine['content_id']}).getText()
+                # print("html", html)
+
+                score_from_question = MillionareHero.get_score_from_question(html, options)
+                print(score_from_question)
+                for k in score_from_question:
+                    self.score[k] += score_from_question[k]
+                print("抓完了...", question, time.time() - t1)
+
+    @staticmethod
+    def cut(string):
+        return set(jieba.cut_for_search(string, HMM=True))
+
+    @staticmethod
+    def get_keyword(string):
+        return set(analyse.tfidf(string))
+
+    @staticmethod
+    def get_score_from_question(html, options):
+        result = {k: 0 for k in options}
+        positions = {k: html.count(options[k]) for k in options}
+        sorted_positions = sorted(positions.items(), key=lambda x: x[1])
+        for i, v in enumerate(sorted_positions):
+            if v[1] != 0:
+                result[v[0]] += i * 20
+        return result
+
+    @staticmethod
+    def get_score_from_option(html, seg_question):
+        cnt = 0
+
+        for q in seg_question:
+            if html.find(q) >= 0:
+                cnt += 1
+                print(q)
+        print(cnt / len(seg_question) * 50)
+        return cnt / len(seg_question) * 50
+
+
+async def get_handler(request):
+    start_time = time.time()
+    data = await request.json()
+    m = MillionareHero(data['question'], data['options'], engine=BAIDU)
+    score = await m.get_score()
+    response = "%s" % json.dumps(sorted(score.items(), key=lambda x: x[1], reverse=True))
+    print("共耗时", time.time() - start_time)
+
+    return web.Response(body=response.encode('utf-8'))
+
+
+async def start(loop):
+    app = web.Application(loop=loop)
+    app.router.add_post('/m/', get_handler)
+    # web.run_app(app, host='0.0.0.0', port=8995)
+    srv = await loop.create_server(app.make_handler(), '0.0.0.0', 8995)
+    print('Server started at http://127.0.0.1:8995...')
+    return srv
+
+
+if __name__ == "__main__":
+    jieba.load_userdict('dict.big.txt')
+    start_time = time.time()
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(start(loop))
+    loop.run_forever()
